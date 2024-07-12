699f9796ac58272a7a9a1115b28bebcde8d9385c
tigoblackdragon <eng.ataghyan@gmail.com>
2024-07-12 15:08:57 +0300

    sec commit before API

--- JS/main.js
+++ JS/main.js
@@ -3,6 +3,7 @@ function openNav() {
     document.getElementById("main").style.left = "250px";
     document.getElementById("opn_btn").classList.add("visually-hidden");
     document.getElementById("cls_btn").classList.remove("visually-hidden");
+    document.getElementById("sch").style.top="0";
   }
   
   function closeNav() {
@@ -10,6 +11,7 @@ function openNav() {
     document.getElementById("main").style.left = "0px";
     document.getElementById("opn_btn").classList.remove("visually-hidden");
     document.getElementById("cls_btn").classList.add("visually-hidden");
+    document.getElementById("sch").style.top="200px";
   }
   document.getElementById("opn_btn").addEventListener("click",openNav);
   document.getElementById("cls_btn").addEventListener("click",closeNav);
